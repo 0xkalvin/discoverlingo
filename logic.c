f572bcf756c7e0963c738e4ffe2e34b9f9174a72
kalvin <kalvinvini1998@gmail.com>
2019-03-28 10:00:01 -0300

    all letters

--- logic.c
+++ logic.c
@@ -12,11 +12,6 @@ void find_language(char text[MAX_LEN]){
     int num_of_chars = number_of_valid_char(text);
     printf("\n the number of valid chars is %d \n", num_of_chars);
 
-    // double *percentual = letter_percentual(letters_frequency, num_of_chars);
-    // for(int i = 0; i < num_of_chars; i++) printf(" %0.2f ", percentual[i] );
-    // printf("\n");
-    // for(int i = 0; i < num_of_chars; i++) printf(" %4c ", letters_frequency[i].l );
-    // printf("\nKKKKKKK %c", portuguese_letters[1]);
 
     double p = 0;
     double e = 0;
@@ -81,6 +76,50 @@ void find_language(char text[MAX_LEN]){
         else if(letters_frequency[i].l == 'n'){
             p += 1.04;
             e += 6.749;
+        }
+        else if(letters_frequency[i].l == 'o'){
+            p += 10.73;
+            e += 7.507;
+        }
+        else if(letters_frequency[i].l == 'p'){
+            p += 2.52;
+            e += 1.929;
+        }
+        else if(letters_frequency[i].l == 'r'){
+            p += 6.53;
+            e += 5.987;
+        }
+        else if(letters_frequency[i].l == 's'){
+            p += 7.81;
+            e += 6.327;
+        }
+        else if(letters_frequency[i].l == 't'){
+            p += 2.52;
+            e += 9.056;
+        }
+        else if(letters_frequency[i].l == 'u'){
+            p += 4.63;
+            e += 2.758;
+        }
+        else if(letters_frequency[i].l == 'v'){
+            p += 1.67;
+            e += 0.978;
+        }
+        else if(letters_frequency[i].l == 'w'){
+            p += 0.01;
+            e += 2.360;
+        }
+        else if(letters_frequency[i].l == 'x'){
+            p += 0.21;
+            e += 0.150;
+        }
+        else if(letters_frequency[i].l == 'y'){
+            p += 0.01;
+            e += 1.974;
+        }
+        else if(letters_frequency[i].l == 'z'){
+            p += 0.47;
+            e += 0.074;
         }
     }
     if(p > e){
