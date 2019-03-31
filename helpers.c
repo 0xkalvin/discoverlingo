796f99b320814ecd2c5a7554387e7247ceaaf3e2
kalvin <kalvinvini1998@gmail.com>
2019-03-31 18:42:22 -0300

    to lowercase

--- helpers.c
+++ helpers.c
@@ -1,49 +1,5 @@
 #include "config.c"
 
-void sort_letter_array(Letter array[], int total_letters){
-    int min;
-    for(int i = 0; i < total_letters; i++){
-        min = i;
-        for(int j = i + 1; j < total_letters; j++){
-            if(array[min].q < array[j].q){
-                min = j;
-            }
-        }
-        Letter temp = array[min];
-        array[min] = array[i];
-        array[i] = temp;
-    }
-}
-
-Letter *all_letters_frequency(char string[MAX_LEN]){
-    int i = 0;
-    int total_letters = 26;
-    int letter_value = 0;
-    Letter *letters_frequency = malloc(total_letters*sizeof(Letter));
-
-    for (int j = 0; j < total_letters; j++) letters_frequency[j].q = 0;
-    for (int j = 0; j < total_letters; j++) letters_frequency[j].l = 'a' + j;
-    
-    while(string[i] != '\0'){ 
-        if(string[i] >= 'a' && string[i] <= 'z'){
-            letter_value = string[i] - 'a';
-            letters_frequency[letter_value].q++;
-        }
-        i++;
-    }
-
-    //sort_letter_array(letters_frequency, total_letters);
-
-    return letters_frequency;
-}
-
-void print_letter_array(Letter *letters_frequency){
-    printf("\n");
-    for(int i = 0; i < 26; i++){
-        printf(" %c = %d \n",letters_frequency[i].l , letters_frequency[i].q);
-    }
-}
-
 int number_of_valid_char(char string[MAX_LEN]){
     int n = 0;
     int i = 0;
@@ -57,5 +13,56 @@ int number_of_valid_char(char string[MAX_LEN]){
 }
 
 bool is_valid(char string[MAX_LEN]){
-    return (number_of_valid_char(string) > 4 && number_of_valid_char(string) < 281) ? true : false;    
+    return (number_of_valid_char(string) > 29 && number_of_valid_char(string) < 10001) ? true : false;    
 }
+
+void print_letter_array(Letter *letters_frequency){
+    printf("\n");
+    for(int i = 0; i < 26; i++){
+        printf(" %c = %d \n",letters_frequency[i].letter , letters_frequency[i].quantity);
+    }
+}
+
+void sort_letter_array(Letter array[], int num_of_letters){
+    int min;
+    for(int i = 0; i < num_of_letters; i++){
+        min = i;
+        for(int j = i + 1; j < num_of_letters; j++){
+            if(array[min].quantity < array[j].quantity){
+                min = j;
+            }
+        }
+        Letter temp = array[min];
+        array[min] = array[i];
+        array[i] = temp;
+    }
+}
+
+
+
+Letter *all_letters_frequency(char string[MAX_LEN]){
+    int i = 0;
+    int num_of_letters = 26;
+    int letter_value = 0;
+    Letter *letters_frequency = malloc(num_of_letters*sizeof(Letter));
+
+    for (int j = 0; j < num_of_letters; j++) letters_frequency[j].quantity = 0;
+    for (int j = 0; j < num_of_letters; j++) letters_frequency[j].letter = 'a' + j;
+
+    for (int j = 0; j < number_of_valid_char(string); j++) string[j] = tolower(string[j]);
+
+    while(string[i] != '\0'){ 
+        if(string[i] >= 'a' && string[i] <= 'z'){
+            letter_value = string[i] - 'a';
+            letters_frequency[letter_value].quantity++;
+        }
+        i++;
+    }
+
+    return letters_frequency;
+}
+
+
+
+
+
